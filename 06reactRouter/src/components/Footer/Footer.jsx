ab14470920132a8edbcf7075a90f434de5f6240c
ANUJ MAURYA <mauryaanuj0011@gmail.com>
2024-07-07 23:10:17 +0530

    change in reactRouter

--- 06reactRouter/src/components/Footer/Footer.jsx
+++ 06reactRouter/src/components/Footer/Footer.jsx
@@ -71,11 +71,11 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
+                        ©2023 
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                            { } mauryaanuj
                         </a>
-                        . All Rights Reserved.
+                        .{} All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
                         <Link to="#" className="text-gray-500 hover:text-gray-900">
